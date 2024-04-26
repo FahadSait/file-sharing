68464114581c91203a6796c3cfd2d605b89c9e16
FahadSait <128489852+FahadSait@users.noreply.github.com>
2024-04-26 18:29:13 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'http://localhost:8000';  
 export const uploadFile = async(data)=> {
     try {
-        let response = await axios.post(`file-sharing-brown.vercel.app/upload`,data);
+        let response = await axios.post(`https://file-sharing-brown.vercel.app//upload`,data);
         return response.data;
     } catch (error) {
         console.error('Error while calling api', error.message);
