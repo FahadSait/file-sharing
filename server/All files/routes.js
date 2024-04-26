e5bd29ccc1fb6a1f7877ae3283a6628505b53fcb
FahadSait <128489852+FahadSait@users.noreply.github.com>
2024-04-26 18:14:15 +0530

    Delete server/All files directory

--- server/All files/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {uploadImage,downloadImage } from '../controller/image.js';
-import upload from '../utis/upload.js';
-
-const router = express.Router();
-
-router.post('/upload', upload.single('file'),uploadImage);
-router.get('/file/:fileId',downloadImage);
-export default router;
